d0538eec42694eb16c74cb1204b94e4e2214007b
JEENA011 <90309557+JEENA011@users.noreply.github.com>
2021-11-22 16:09:22 +0530

    Add files via upload

---
+++ 3_Implementation/inc/header.h
@@ -0,0 +1,26 @@
+/**
+ * @file header.h
+ * @author Manish Jeena
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-21
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef HEADER_H
+#define HEADER_H
+
+struct CustomerDetails   //STRUCTURE DECLARATION
+{
+	char roomnumber[10];
+	char name[20];
+	char address[25];
+	char phonenumber[15];
+	char nationality[15];	
+	char email[20];
+	char period[10];
+	char arrivaldate[10];
+}s;
+
+#endif //#ifndef HEADER_H
